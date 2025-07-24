5e414b023ee2b4a52d59cd83dd4b14fa82fd8dbd
Zanna-dev <pamn2704@gmail.com>
2025-07-24 22:12:54 +0100

    Create Custom Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ProfileComponent } from './profile/profile.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [LoginComponent, SignupComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  // title = 'Code Step By Step';
+  title = 'Code Step By Step';
   name = "Anil Sidhu";
   data = 100;
   data2 = 200;
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import { Component} from "@angular/core";
+
+@Component({
+    selector: 'app-profile',
+    // template:"<h1>Profile Component</h1>",
+    styleUrl:'./profile.component.css',
+    templateUrl:'./profile.component.html'
+})
+export class ProfileComponent{
+
+}
