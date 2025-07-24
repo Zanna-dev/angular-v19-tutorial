5e5b1892c9428fe9ef6f9484cdb2b4e160a15afb
Zanna-dev <pamn2704@gmail.com>
2025-07-24 22:47:49 +0100

    Define Data Type in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,15 +7,45 @@ import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, SignupComponent, ProfileComponent],
+  // imports: [LoginComponent, SignupComponent, ProfileComponent],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Code Step By Step';
-  name = "Anil Sidhu";
-  data = 100;
-  data2 = 200;
-  user1 = "Anil";
-  user2 = "John"
+  name:string = "sidhu";
+  data:number | string = 30;
+  userLogin:boolean = true;
+  info:any = "this is some data";
+  email="ancestest.com"
+
+  apple(){
+    this.data = "abc";
+    this.info = 100;
+
+    this.info = true
+    this.info = []
+  }
+
+  sum(a:number,b:number){
+    let x : number | string = 10
+  console.log(a+b);
+  }
 }
+
+
+// name = "Anil Sidhu";
+  // data = 100;
+  // data2 = 200;
+  // user1 = "Anil";
+  // user2 = "John"
+
+  // handleClickEvent() {
+  //   console.log("function called");
+  //   this.handleClickEvent()
+  // }
+
+  // otherFunction(){
+  //   console.log("other function")
+  // }
