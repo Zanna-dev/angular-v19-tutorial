445652bb15728f7b4804df93fb1d381a3abfa563
Zanna-dev <pamn2704@gmail.com>
2025-07-24 23:30:12 +0100

    Make Counter App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,26 +13,44 @@ import { ProfileComponent } from './profile/profile.component';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Code Step By Step';
-  name:string = "sidhu";
-  data:number | string = 30;
-  userLogin:boolean = true;
-  info:any = "this is some data";
-  email="ancestest.com"
-
-  apple(){
-    this.data = "abc";
-    this.info = 100;
-
-    this.info = true
-    this.info = []
+  count:number = 0
+  handleIncrement(){
+    this.count = this.count + 1
   }
 
-  sum(a:number,b:number){
-    let x : number | string = 10
-  console.log(a+b);
+   handleDecrement(){
+    this.count = this.count - 1
   }
+
+   handleReset(){
+    this.count = 0
+  }
+
+  handleCounter(val:string){
+    if(val=='minus'){
+      this.count=this.count-1
+    }else if(val=='plus'){
+      this.count=this.count+1
+    }else{
+      this.count = 0
+    }
+  }
+   title = 'Code Step By Step';
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 // name = "Anil Sidhu";
@@ -48,4 +66,24 @@ export class AppComponent {
 
   // otherFunction(){
   //   console.log("other function")
+  // }
+
+   
+  // name:string = "sidhu";
+  // data:number | string = 30;
+  // userLogin:boolean = true;
+  // info:any = "this is some data";
+  // email="ancestest.com"
+
+  // apple(){
+  //   this.data = "abc";
+  //   this.info = 100;
+
+  //   this.info = true
+  //   this.info = []
+  // }
+
+  // sum(a:number,b:number){
+  //   let x : number | string = 10
+  // console.log(a+b);
   // }
