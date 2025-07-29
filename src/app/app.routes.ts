b64d9f68dc46942f504a25cfdf970053000e51ba
Zanna-dev <pamn2704@gmail.com>
2025-07-29 13:25:30 +0100

    Add Page Not Found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,12 @@ import { LoginComponent } from './login/login.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 export const routes: Routes = [
     {path:'login', component:LoginComponent},
     {path:'about', component:AboutComponent},
     {path:'contact', component:ContactComponent},
     {path:'', component:HomeComponent},
-
+    {path:'**', component:PageNotFoundComponent},
 ];
